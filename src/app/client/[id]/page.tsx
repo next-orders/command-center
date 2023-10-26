1dda9458c4f46e83effda759ca80245112074766
Nick Kosarev <hmbanan666@hotmail.com>
2023-10-26 18:04:59 +0200

    fix Loyalty bar for mobile

--- src/app/client/[id]/page.tsx
+++ src/app/client/[id]/page.tsx
@@ -164,8 +164,8 @@ const LoyaltyProgress = ({ percent }: LoyaltyProgressProps) => {
   if (percent < 10) percent = 10;
 
   return (
-    <div className="w-full md:group-hover:scale-105 duration-300">
-      <div className="w-28 h-4 mx-auto bg-zinc-50 rounded-xl drop-shadow-md hover:scale-125 duration-200">
+    <div className="relative w-full md:group-hover:scale-105 duration-300">
+      <div className="relative w-28 h-4 mx-auto bg-zinc-50 rounded-xl drop-shadow-md hover:scale-125 duration-200">
         <div
           className={`absolute bottom-0 left-0 h-4 bg-indigo-500 rounded-xl`}
           style={{ width: `${percent}%` }}
