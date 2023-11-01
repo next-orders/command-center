d3e1c244c75fe00b5079033eb66ca267948878fd
Nick Kosarev <hmbanan666@hotmail.com>
2023-11-01 20:45:18 +0200

    error and not-found

--- src/client/api.ts
+++ src/client/api.ts
@@ -27,7 +27,7 @@ export const GetAllMedia = async () => {
     },
   });
   if (media instanceof Error) {
-    if (media.message.includes("401")) {
+    if (media.message.includes("Unauthorized")) {
       throw new Error("You have no required Permissions: READ_MEDIA");
     }
     throw new Error("Unknown");
